4b6001b0cdb197c0c957f079f9c7e66639edbd12
Tyler Nijmeh <tylernij@gmail.com>
2022-07-27 13:32:20 -0700

    Add basic field getting and field get tests
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/yamlcfg.dart
+++ lib/yamlcfg.dart
@@ -1,4 +1,43 @@
+import 'package:yaml/yaml.dart';
+
 /// A type-safe configuration file parser with support for YAML notation.
 class YamlCfg {
+  /// Creates a new [YamlCfg] given some [content] data.
+  YamlCfg(this.content) {
+    final _unsafeYamlMap = loadYaml(content) as YamlMap?;
+    if (_unsafeYamlMap == null) {
+      throw ArgumentError(
+        'No content given',
+        'content',
+      );
+    }
 
+    _yamlMap = _unsafeYamlMap;
+  }
+
+  /// A string representation of the YAML file's content.
+  final String content;
+
+  /// A [YamlMap] generated from the [content].
+  late final YamlMap _yamlMap;
+
+  /// Retrieve a field of type [T] given a [name].
+  T get<T>(String name) {
+    final fieldValue = _yamlMap[name];
+
+    // Reject missing fields.
+    if (fieldValue == null) {
+      throw ArgumentError('No field found', name);
+    }
+
+    // Reject mismatching field types.
+    if (fieldValue is! T) {
+      throw ArgumentError(
+        'Field type mismatch; $T != ${fieldValue.runtimeType}',
+        name,
+      );
+    }
+
+    return fieldValue;
+  }
 }
--- test/yamlcfg_test.dart
+++ test/yamlcfg_test.dart
@@ -1,3 +1,29 @@
+import 'package:test/test.dart';
+import 'package:yaml/yaml.dart';
+import 'package:yamlcfg/yamlcfg.dart';
+
 void main() {
-  // TODO(tytydraco): tests
+  group('YamlCfg', () {
+    test('Empty YAML', () {
+      expect(() => YamlCfg(''), throwsArgumentError);
+    });
+
+    test('Get field as int', () {
+      expect(YamlCfg('test: 5').get<int>('test'), 5);
+    });
+
+    test('Get field as YamlMap', () {
+      expect(
+        YamlCfg('test:\n  val: 1').get<YamlMap>('test'),
+        YamlMap.wrap({'val': 1}),
+      );
+    });
+
+    test('Get field with wrong type', () {
+      expect(
+        () => YamlCfg('test: 5').get<String>('test'),
+        throwsArgumentError,
+      );
+    });
+  });
 }
